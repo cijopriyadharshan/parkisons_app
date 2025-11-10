2c225dae94b9347114d1148893787b2844288b79
cijopriyadharshan <cijopriyadharshan2@gmail.com>
2025-11-11 01:16:49 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,12 +1,11 @@
 # run.py
-from app import fastapi_app
+from app import app  # Flask app
+from threading import Thread
 import uvicorn
 
+def run_fastapi():
+    uvicorn.run("app:fastapi_app", host="0.0.0.0", port=8000, log_level="error")
+
 if __name__ == "__main__":
-    uvicorn.run(
-        "app:fastapi_app",
-        host="0.0.0.0",
-        port=8000,
-        workers=1,
-        timeout_keep_alive=180
-    )
+    Thread(target=run_fastapi, daemon=True).start()
+    app.run(host="0.0.0.0", port=5000, debug=False)
