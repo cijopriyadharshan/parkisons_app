6ff633b9c2ed7c0121523f8361be7225a835edc6
cijopriyadharshan <cijopriyadharshan2@gmail.com>
2025-11-11 17:31:03 +0530

    Update run.py

--- run.py
+++ run.py
@@ -4,4 +4,12 @@ from app import app
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=False)
+    try:
+        import subprocess
+        subprocess.run(["gunicorn", "--version"], check=True, capture_output=True)
+        import sys
+        sys.argv = ["gunicorn", "-w", "2", "-b", f"0.0.0.0:{port}", "app:app"]
+        import gunicorn.app.wsgiapp
+        gunicorn.app.wsgiapp.run()
+    except:
+        app.run(host="0.0.0.0", port=port)
